adc0bd4da0ad2656e28e88e7f4045b32ea2ee071
dmitriy <dmitriy1a22222@gmail.com>
2019-01-25 22:54:17 +0200

    change bag

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,8 @@ gulp.task('img', function() {
 gulp.task('build', ['clean', 'img', 'sass'], function() {
 
 	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-		'src/css/main.css'
+		'src/css/main.css',
+		'src/css/main.min.css'
 		])
 	.pipe(gulp.dest('dist/css'))
 
